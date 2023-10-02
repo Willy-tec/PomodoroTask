1a3a72090cdcdb855f2357991a05944abcded73f
Willy-tec <alown666@gmail.com>
2023-10-02 17:45:54 +0200

    Creation of the quoting components

---
+++ src/components/Quoting.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import quotes from '../assets/compliment.json'
+
+const quote = ref(quotes[Math.floor(Math.random() * quotes.length)])
+</script>
+
+<template>
+    <div class="card">
+        <h1>{{ quote.phrase }}({{ quote.id }})</h1>
+        <button type="button" @click="quote = quotes[Math.floor(Math.random() * quotes.length)]">New Quote</button>
+    </div>
+</template>
+
+
+<style scoped>
+.card{
+    border: 1px solid black;
+    border-radius: 4px;
+    font-size: .5rem;
+    background-color: azure;
+    color: black;
+}
+</style>
